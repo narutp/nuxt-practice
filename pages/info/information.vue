c522453cdbd1077cb9a31bd572259e0a37b07a21
Narut Poovorakit <net.narut.p@gmail.com>
2017-07-07 16:18:08 +0700

    Try another way to route

---
+++ pages/info/information.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class = "container">
+    <h1>Information... testing 2 button with different code that route to home page</h1>
+    <button type="primary" @click="homeClicked()">Home1</button>
+    <button type="primary" @click="homeClicked2()">Home2</button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    homeClicked () {
+      this.$router.replace({ path: '/' })
+    },
+    homeClicked2 () {
+      window.location.href = '/'
+    }
+  }
+}
+</script>
--- pages/page2.vue
+++ pages/page2.vue
@@ -3,10 +3,12 @@
     <h2>This is PAGE 2</h2>
     <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-    <div class="button--blue"><nuxt-link to="/">Back</nuxt-link></div>
+    <div style="text-align: center">
+      <div class="button--blue"><nuxt-link to="/">Back</nuxt-link></div>
+      <div class="button--red"><nuxt-link to="/info/information">Information</nuxt-link></div>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
 }
